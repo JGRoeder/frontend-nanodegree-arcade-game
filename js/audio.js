214dba8808e793d7667d9788db96c7f9cfbee33d
JGRoeder <j.g.roeder@gmail.com>
2015-02-09 11:48:44 -0500

    fix audio preloading, fix comments

--- js/audio.js
+++ js/audio.js
@@ -1,81 +1,80 @@
-/* Resources.js
- * This is simple an image loading utility. It eases the process of loading
- * image files so that they can be used within your game. It also includes
- * a simple "caching" layer so it will reuse cached images if you attempt
- * to load the same image multiple times.
+/* audio.js
+ * Primarily outright stolen from the resources.js provided by udacity
+ * Thanks Gents and/or Ladies!
  */
 (function() {
     var audioCache = {};
     var loading = [];
     var readyCallbacks = [];
 
-    /* This is the publicly accessible image loading function. It accepts
-     * an array of strings pointing to image files or a string for a single
-     * image. It will then call our private image loading function accordingly.
+    /* This is the publicly accessible loading function. It accepts
+     * an array of strings pointing to audio files or a string for a single
+     * audio file. It will then call our private audio loading function accordingly.
      */
     function load(urlOrArr) {
         if(urlOrArr instanceof Array) {
-            /* If the developer passed in an array of images
-             * loop through each value and call our image
-             * loader on that image file
+            /* If the developer passed in an array of urls
+             * loop through each value and call our audio
+             * loader on that audio file
              */
             urlOrArr.forEach(function(url) {
                 _load(url);
             });
         } else {
             /* The developer did not pass an array to this function,
-             * assume the value is a string and call our image loader
+             * assume the value is a string and call our audio loader
              * directly.
              */
             _load(urlOrArr);
         }
     }
 
-    /* This is our private image loader function, it is
-     * called by the public image loader function.
+    /* This is our private audio file loader function, it is
+     * called by the public audio file loader function.
      */
 
      function _load(url) {
 
          if(audioCache[url]) {
              /* If this URL has been previously loaded it will exist within
-              * our resourceCache array. Just return that image rather
-              * re-loading the image.
+              * our resourceCache array. Just return that audio file rather
+              * re-loading the audio file.
               */
              return audioCache[url];
          } else {
              /* This URL has not been previously loaded and is not present
-              * within our cache; we'll need to load this image.
+              * within our cache; we'll need to load this audio file.
               */
-            var audio = new Audio();
-            audio.oncanplaythrough = function() {
-              audioCache[url] = audio;
-              // if(isReady()) {
-              //     readyCallbacks.forEach(function(func) { func(); });
-              // }
+            var myAudio = new Audio();
+            myAudio.oncanplaythrough = function() {
+              console.log(audioCache[url]);
+              audioCache[url] = myAudio;
+
+              // Shutting up callbacks for now
+              //  if(isReady()) {
+              //      readyCallbacks.forEach(function(func) { func(); });
+              //  }
             };
 
              /* Set the initial cache value to false, this will change when
-              * the image's onload event handler is called. Finally, point
-              * the images src attribute to the passed in URL.
+              * the oncanplaythrough event handler is called. Finally, point
+              * the audio src attribute to the passed in URL.
               */
 
-             audioCache[url] = audio;
-             audio.src = url;
-             //console.log(audioCache)
-             //console.log(audio.src)
+             audioCache[url] = false;
+             myAudio.src = url;
          }
      }
 
-    /* This is used by developer's to grab references to images they know
-     * have been previously loaded. If an image is cached, this functions
+    /* This is used by developer's to grab references to audio files they know
+     * have been previously loaded. If an audio is cached, this functions
      * the same as calling load() on that URL.
      */
     function get(url) {
         return audioCache[url];
     }
 
-    /* This function determines if all of the images that have been requested
+    /* This function determines if all of the audio files that have been requested
      * for loading have in fact been completed loaded.
      */
     function isReady() {
@@ -90,7 +89,7 @@
     }
 
     /* This function will add a function to the callback stack that is called
-     * when all requested images are properly loaded.
+     * when all requested audio files are properly loaded.
      */
     function onReady(func) {
         readyCallbacks.push(func);
@@ -99,7 +98,6 @@
     /* This object defines the publicly accessible functions available to
      * developers by creating a global Resources object.
      */
-
     window.AudioResources = {
         load: load,
         get: get,
